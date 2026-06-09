abf977caf57f2f7ce645a388538ca4f33a1cb3e2
pdchauhan003 <pdchauhan003@gmail.com>
2026-06-09 18:04:11 +0530

    update next config

--- multiuserblog/next.config.ts
+++ multiuserblog/next.config.ts
@@ -8,32 +8,56 @@
 // export default nextConfig;
 
 
+// import type { NextConfig } from "next";
+// /** @type {import('next').NextConfig} */
+
+// const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:2321";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'images.unsplash.com',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'res.cloudinary.com',
+//       },
+//     ],
+//   },
+//   reactCompiler: true,
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/api/:path*",
+//         destination: `${BACKEND_URL}/api/:path*`, // Proxy API requests to Express
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
-
-const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:2321";
-
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
       },
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
       },
     ],
-  },
-  reactCompiler: true,
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${BACKEND_URL}/api/:path*`, // Proxy API requests to Express
-      },
-    ];
   },
 };
 
